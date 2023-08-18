b240018ed33ce4db1ffbe2d86743be44038b6740
osparking <jbpark03@gmail.com>
2023-08-18 10:50:18 +0900

    security filter chain bean

--- src/main/java/space/bum/c6_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c6_ex1/config/SecurityConfig.java
@@ -2,14 +2,25 @@ package space.bum.c6_ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.httpBasic()
+				.and()
+				.authorizeRequests()
+					.anyRequest().authenticated()
+				.and().build();
+	}
 	
 	@Bean
 	UserDetailsService userDetailsService() {
